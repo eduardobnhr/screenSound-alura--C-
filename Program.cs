425ed0a6bee2048508ebcff9364738952a8fc3e8
eduardobnhr <eduardobnhr@gmail.com>
2023-11-18 10:34:26 -0300

    criando uma lista para bandas

--- Program.cs
+++ Program.cs
@@ -23,6 +23,28 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opcao: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica) 
+    {
+        case 1: Console.WriteLine("voce escolheu a opcao " + opcaoEscolhidaNumerica);
+            break; 
+        case 2: Console.WriteLine("voce escolheu a opcao " + opcaoEscolhidaNumerica);
+                break;
+        case 3: Console.WriteLine("voce escolheu a opcao " + opcaoEscolhidaNumerica);
+            break;
+        case 4: Console.WriteLine("voce escolheu a opcao " + opcaoEscolhidaNumerica);
+            break;
+        case -1:
+            Console.WriteLine("voce escolheu a opcao " + opcaoEscolhidaNumerica);
+            break;
+        default: Console.WriteLine("Opcao invalida");
+            break;
+    } 
+
 }
 
 ExibirMensagemDeBoasVindas();
